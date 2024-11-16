bf5c20eabc14ac12235853724715b84067527ed9
felixbc0 <93590906+felixbc0@users.noreply.github.com>
2024-11-16 23:59:55 +0100

    feat: add basic solutions.rs

---
+++ workshop/solutions.rs
@@ -0,0 +1,27 @@
+// Library Management System Exercises Solutions
+
+pub fn main() {}
+
+// Common Programming Concepts – refresher
+
+// Immutability & Everything non-nullable by default
+
+// Ownership & Borrow Checker
+
+// Macros
+
+// Lifetimes
+
+// Box
+
+// Structs & Option Struct
+
+// Enums & Pattern Matching
+
+// (Smart Pointer)
+
+// (Error Handling)
+
+// (Generics=Traits)
+
+// ((Async Rust))
